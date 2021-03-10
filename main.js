effb6641e4dfff847779a29ea03aba67f6c190c3
Sanjiv chaudhary <sanjivchudhary416@gmail.com>
2021-03-10 23:16:43 +0545

    generated random number

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 let player1score =0;
 let player2score =0;
- let player1Turn =true;
- 
+let player1Turn =true;
+ // create variable to store references of necesary dom nodes
+
+ const player1Dice = document.getElementById('player1Dice');
+ const player2Dice = document.getElementById('player2Dice');
+ const player1scoreboard =document.getElementById('player1scoreboard');
+ const player2scoreboard =document.getElementById('player2scoreoard');
+ const massage= document.getElementById('massage');
+const resetBtn= document.getElementById('resetBtn');
+const rollBtn= document.getElementById('rollBtn');
+
+/*hook up a click event listner to roll dice button . log out a random number between 1 and 6*/
+ rollBtn.addEventListener("click",function(){
+ 	const randomNumber = Math.floor(Math.random()*6)+1;// *6 is random number from 0-5 and +1 makes 1-6.
+ 	console.log(randomNumber);
+ })
